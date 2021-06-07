2a67a389dba9413cd5a0759080fb6e7d83815c24
manvantar <manukvshetty@gmail.com>
2021-06-08 00:06:02 +0530

    feat: swagger authorization Token for API PUT,GET,DELETE

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dbconnect = require('./config/database.js');
 const logger = require('./logger/loggerCofig');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger-output.json');
+
 require('../EmployeePayroll_app/server')
 
 // create express app
@@ -18,7 +19,13 @@ app.use(express.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(express.json())     
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+var options = {
+    swaggerOptions:{
+        url: 'http://petstore.swagger.io/v2/swagger.json'
+  }
+}
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
 
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EmployeePlayRoll application Backend"});
