a21145ffc53c0f612690674dc57a5fcd3660d1bc
manvantar <manukvshetty@gmail.com>
2021-07-21 20:55:13 +0530

    feat: adding eslint and indenting and defining rules

--- app/routes/employee.js
+++ app/routes/employee.js
@@ -1,27 +1,27 @@
+/* eslint-disable import/extensions */
 const controllerEmployee = require('../controllers/employee.js');
 const helper = require('../middleware/helper.js');
 const userController = require('../controllers/user.js');
 
 module.exports = (app) => {
+  // Create a new employee
+  app.post('/add/employee', helper.checkToken, controllerEmployee.create);
 
-    // Create a new employee
-    app.post('/add/employee', helper.checkToken, controllerEmployee.create);
+  // Retrieve all employees
+  app.get('/employees', helper.checkToken, controllerEmployee.findAllEmployees);
 
-    // Retrieve all employees
-    app.get('/employees', helper.checkToken, controllerEmployee.findAllEmployees);
+  // Retrieve a single employee with employeeId
+  app.get('/employees/:employeeId', helper.checkToken, controllerEmployee.findOneData);
 
-    // Retrieve a single employee with employeeId
-    app.get('/employees/:employeeId', helper.checkToken, controllerEmployee.findOneData);
+  // Update a employee with employeeId
+  app.put('/update/employee/:employeeId', helper.checkToken, controllerEmployee.update);
 
-    // Update a employee with employeeId
-    app.put('/update/employee/:employeeId', helper.checkToken, controllerEmployee.update);
+  // Delete a employee with employeeId
+  app.delete('/delete/employee/:employeeId', helper.checkToken, controllerEmployee.delete);
 
-    // Delete a employee with employeeId
-    app.delete('/delete/employee/:employeeId', helper.checkToken, controllerEmployee.delete);
+  // create user with emailId and Password
+  app.post('/registration', userController.create);
 
-    // create user with emailId and Password
-    app.post('/registration', userController.create);
-
-    // login user emailId and Password
-    app.post('/login', userController.login);
-}
+  // login user emailId and Password
+  app.post('/login', userController.login);
+};
--- config/database.js
+++ config/database.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
 
 /*
- * function to connect mongoose database 
+ * function to connect mongoose database
  * @returns connection
  */
-function dbconnect(){
+function dbconnect() {
+  // eslint-disable-next-line no-unused-expressions
+  mongoose.promise;
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 
-    mongoose.promise;
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    });
-
-    return mongoose.connection
+  return mongoose.connection
+    // eslint-disable-next-line no-console
     .once('open', () => console.log('Mongo database Connected'))
-    .on('error', (error)=> {
-        console.log("Eroor found",error)
+    .on('error', (error) => {
+      // eslint-disable-next-line no-console
+      console.log('Eroor found', error);
     });
 }
 
-module.exports=dbconnect;
+module.exports = dbconnect;
